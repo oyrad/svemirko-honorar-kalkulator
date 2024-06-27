4f7fe9a5066b6e0fae9b4261982dd8a1f67d73b2
Dario Susanj <dario.susanj@tradelocker.com>
2024-06-27 09:26:08 +0200

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: 'kalkulator by darac',
   description: 'calculate ur gains',
+  icons: {
+    icon: '/favicon.png',
+  },
 };
 
 export default function RootLayout({
