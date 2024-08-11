d92847a955b7608386618adc8c483a7305fd69ce
oyrad <dario.susanj2@gmail.com>
2024-08-11 11:34:02 +0200

    add react query

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { ReactNode } from 'react';
 import NextTopLoader from 'nextjs-toploader';
+import Providers from '@/app/_components/Providers';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -23,9 +24,11 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NextTopLoader showSpinner={false} />
-        <main className="space-y-4 xl:px-[24rem] lg:px-[20rem] md:px-[16rem] px-8 mt-8 max-w-[1280px] mx-auto mb-8">
-          {children}
-        </main>
+        <Providers>
+          <main className="space-y-4 xl:px-[24rem] lg:px-[20rem] md:px-[16rem] px-6 mt-8 max-w-[1280px] mx-auto mb-8">
+            {children}
+          </main>
+        </Providers>
       </body>
     </html>
   );
