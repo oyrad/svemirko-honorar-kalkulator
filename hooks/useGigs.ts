b428b5a84f740718da27cecd86d2412a49b08337
oyrad <dario.susanj2@gmail.com>
2024-09-05 19:31:01 +0200

    add cache time 0

--- hooks/useGigs.ts
+++ hooks/useGigs.ts
@@ -1,8 +1,14 @@
 import { useQuery } from 'react-query';
 
 export function useGigs() {
-  return useQuery('gigs', async () => {
-    const res = await fetch('/api/gigs');
-    return res.json();
-  });
+  return useQuery(
+    'gigs',
+    async () => {
+      const res = await fetch('/api/gigs');
+      return res.json();
+    },
+    {
+      cacheTime: 0,
+    },
+  );
 }
