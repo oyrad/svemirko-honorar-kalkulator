ca9b48d282266cdbcabf633d8f2566441fd5e873
oyrad <dario.susanj2@gmail.com>
2025-01-06 13:35:48 +0100

    feat: add net royalties calc for each member

--- utils/royalties-utils.ts
+++ utils/royalties-utils.ts
@@ -1,28 +1,25 @@
-import { Expense } from '@/types/types';
+import { Expense, Split } from '@/types/types';
+import { getRates } from '@/utils/get-rates';
 
-export function getTotalExpenses(expenses: Expense[]) {
-  return expenses.reduce((acc, curr) => acc + parseFloat(curr.amount), 0);
+export function getTotalExpenses(expenses: Array<Expense>) {
+  return expenses.reduce((acc, current) => acc + parseFloat(current.amount), 0);
 }
 
 export function getNetRoyalties(
   grossRoyalties: string,
   isThereBookingFee: boolean,
-  expenses: Expense[],
+  expenses: Array<Expense>,
 ) {
-  const totalBandFee =
-    (parseFloat(grossRoyalties) || 0) * (isThereBookingFee ? 0.9 : 1);
+  const totalBandFee = (parseFloat(grossRoyalties) || 0) * (isThereBookingFee ? 0.9 : 1);
 
-  return expenses.reduce(
-    (acc, expense) => acc - (parseFloat(expense.amount) || 0),
-    totalBandFee,
-  );
+  return expenses.reduce((acc, expense) => acc - (parseFloat(expense.amount) || 0), totalBandFee);
 }
 
 export function getNetRoyaltiesByPerson(
   index: string,
   netRoyalties: number,
   rate: number,
-  expenses: Expense[],
+  expenses: Array<Expense>,
 ) {
   let totalPayWithExpenses = netRoyalties * rate;
   expenses.forEach((expense) => {
@@ -32,4 +29,18 @@ export function getNetRoyaltiesByPerson(
   });
 
   return totalPayWithExpenses;
+}
+
+export function getNetRoyaltiesForAllMembers(
+  netRoyalties: number,
+  expenses: Array<Expense>,
+  split: Split,
+) {
+  const rates = getRates(split);
+
+  const markoPay = getNetRoyaltiesByPerson('1', netRoyalties, rates.MARKO_RATE, expenses);
+  const taliPay = getNetRoyaltiesByPerson('2', netRoyalties, rates.TALI_RATE, expenses);
+  const darioPay = getNetRoyaltiesByPerson('3', netRoyalties, rates.DARIO_RATE, expenses);
+
+  return [markoPay, taliPay, darioPay];
 }
